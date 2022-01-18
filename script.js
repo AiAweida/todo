a5b4e22843fa5b9ca2ed0e4073c59a473b15b1cf
Adib Aweida <AAweida@asaltech.com>
2022-01-18 16:57:11 +0200

    codec

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 function toggleTheme() {
-    const element = document.body;
-    element.classList.toggle("dark");
-  const themeBtn =document.getElementById("themebtn");
- 
- 
- 
-  if(themeBtn.classList.contains("icon-sun")) {
-  themeBtn.classList.remove("icon-sun"),
-  themeBtn.classList.add("icon-moon");
+  const element = document.getElementById("hContain");
+  element.classList.toggle("dark");
+  const themeBtn = document.getElementById("themebtn");
+  if (themeBtn.classList.contains("icon-sun")) {
+    themeBtn.classList.replace("icon-sun", "icon-moon");
+  } else if (themeBtn.classList.contains("icon-moon")) {
+    themeBtn.classList.replace("icon-moon", "icon-sun");
   }
-  else if(themeBtn.classList.contains("icon-moon")){
-  themeBtn.classList.remove("icon-moon") ,
-  themeBtn.classList.add("icon-sun");
-  }
-}
+  
+  const helement = document.getElementById("bContain");
+  helement.classList.toggle("bContainer");
+  
+  } 
+
+function newele(){
+
+  
+
+    }
+
