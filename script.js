9eb76eeaa593fbfad92594277e4220cb0d2a0313
adeeb salameh <aaweida@asaltech.com>
2022-02-01 13:06:52 +0200

    class-changes

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 function toggleTheme() {
-  
+
   const themeBtn = document.getElementById("themebtn");
   if (themeBtn.classList.contains("Icon-Sun")) {
-    themeBtn.classList.replace("icon-Sun", "Icon-Moon");
+    themeBtn.classList.replace("Icon-Sun", "Icon-Moon");
   } else if (themeBtn.classList.contains("Icon-Moon")) {
     themeBtn.classList.replace("Icon-Moon", "Icon-Sun");
   }
   const helement = document.getElementById("bContain");
   helement.classList.toggle("Dark");
+  const ielement = document.getElementById("myInput");
+  ielement.classList.toggle("Dark")
 }
-function newele() {}
+function newele() { }
