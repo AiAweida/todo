fe1677b50e00e9ff2fbbc9d75a0d897abca91d6b
adeeb salameh <aaweida@asaltech.com>
2022-02-06 15:53:28 +0200

    html elements got reorganized css fixed the ul and its elements position the add button placement ot fixed also the script was redundant so i removed redunancy and unnecessary lines

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function toggleTheme() {
-  
+
   const themeBtn = document.getElementById("themebtn");
   if (themeBtn.classList.contains("Icon-Sun")) {
     themeBtn.classList.replace("Icon-Sun", "Icon-Moon");
@@ -8,21 +8,14 @@ function toggleTheme() {
   }
   const helement = document.getElementById("bContain");
   helement.classList.toggle("Dark");
-  const ielement = document.getElementById("myInput");
-  ielement.classList.toggle("Dark")
-  const btnadd = document.getElementById("AddBtn");
-  btnadd.classList.toggle("Dark");
-
-  
 }
-
 function newElement() {
   const li = document.createElement("li");
-  li.className=("Li_Layout");
+  li.className = ("Li_Layout");
   const inputValue = document.getElementById("myInput").value;
   const t = document.createTextNode(inputValue);
   li.appendChild(t);
-  if (inputValue =='') {
+  if (inputValue == '') {
     alert("please fill the text box with letters ");
   } else {
     document.getElementById("UnList").appendChild(li);
