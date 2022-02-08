73977583acf5ee37f21de9a1e001b5a549d73d36
adeeb salameh <aaweida@asaltech.com>
2022-02-08 11:39:20 +0200

    made checked button functional

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ function newElement() {
   const li = document.createElement("li");
   li.className = "Li_Layout";
   li.id = "ListItem";
-  const inputValue = document.getElementById("myInput").value;
-  const t = document.createTextNode(inputValue);
-  li.appendChild(t);
+  const inputValue = document.getElementById("myInput").value.trim();
+  const txt = document.createTextNode(inputValue);
+  li.appendChild(txt);
   if (inputValue === "") {
     alert("please fill the text box with letters ");
   } else {
@@ -24,17 +24,25 @@ function newElement() {
     del.className = "Del__Btn";
     li.appendChild(del);
     del.addEventListener("click", deleteItem);
-    let Chk = document.createElement("input");
+    let Chk = document.createElement("span");
     Chk.type = "checkbox";
     Chk.className = "Chk__Btn";
-    Chk.id = "hkBtn";
-    Chk.addEventListener("click", checkItem);
     li.appendChild(Chk);
+    Chk.addEventListener("click", checkItem);
   }
-  document.getElementById("UnList").value = "";
+  document.getElementById("myInput").value = "";
 }
 
 function deleteItem(event) {
   event.target.parentElement.remove();
 }
-function checkItem(event) {}
+function checkItem(event) {
+  event.target.parentElement.classList.toggle("lined");
+  event.target.classList.toggle("clicked");
+}
+const kp = document.getElementById("myInput");
+kp.addEventListener("keypress", (event) => {
+  if (event.keyCode === 13) {
+    newElement();
+  }
+});
